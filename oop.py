ac365ef37da49028093765642f70a4ecabfe8b28
prawirayuda <muhammadprawirayuda@gmail.com>
2022-01-10 17:42:42 +0700

    konstruktor

--- oop.py
+++ oop.py
@@ -1,1 +1,15 @@
-#
+# constructor
+class Hero:
+    def __init__(self,inputName, inputPower, inputArmor):
+        self.name = inputName
+        self.power = inputPower
+        self.armor = inputArmor
+        # print("he", x)
+
+hero1 = Hero("sniper",12,1)
+print(hero1.name)
+print(hero1.power)
+print(hero1.armor)
+print(hero1.__dict__)# ini untuk menampilkan lengkap isi list nya
+
+# hero2 = Hero()
