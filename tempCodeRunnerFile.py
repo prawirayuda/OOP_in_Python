5d3eda1345464fa4787cbcda6740e7b48b6bfbb8
prawirayuda <muhammadprawirayuda@gmail.com>
2022-01-12 10:48:50 +0700

    Latihan inheritance dan import modul

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,8 +1,2 @@
-        # # print("{}\n\tlevel : {}\n\thealth : {} \n\tpower: {}\n\tarmor : {}".format(
-        #     self.__name,
-        #     self.__level,
-        #     self.__health,
-        #     self.__power,
-        #     self.__armor
-        # # ))      
-        
+
+slader.show_info()
